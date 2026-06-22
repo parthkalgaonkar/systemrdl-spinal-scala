9b22643df2dcc2bcf83653b7a859bd06adb5cb53
Parth Kalgaonkar <parthkalgaoooonkar@gmail.com>
2026-06-22 10:56:18 +0530

    Add IMasterSlave trait to cpuif

--- systemrdl_spinal/src/main/PeakrdlCpuIf.scala
+++ systemrdl_spinal/src/main/PeakrdlCpuIf.scala
@@ -1,18 +1,26 @@
 package systemrdl_spinal
 
 import spinal.core._
+import spinal.lib._
 
-case class PeakrdlCpuIf(addr_width: Int, data_width: Int) extends Bundle {
-  val req = in Bool()
-  val req_is_wr = in Bool()
-  val addr = in Bits(addr_width bits)
-  val wr_data = in Bits(data_width bits)
-  val wr_biten = in Bits(data_width bits)
-  val req_stall_wr = out Bool()
-  val req_stall_rd = out Bool()
-  val rd_ack = out Bool()
-  val rd_err = out Bool()
-  val rd_data = out Bits(data_width bits)
-  val wr_ack = out Bool()
-  val wr_err = out Bool()
+case class PeakrdlCpuIf(
+  addr_width: Int, data_width: Int
+) extends Bundle with IMasterSlave {
+  val req = Bool()
+  val req_is_wr = Bool()
+  val addr = Bits(addr_width bits)
+  val wr_data = Bits(data_width bits)
+  val wr_biten = Bits(data_width bits)
+  val req_stall_wr = Bool()
+  val req_stall_rd = Bool()
+  val rd_ack = Bool()
+  val rd_err = Bool()
+  val rd_data = Bits(data_width bits)
+  val wr_ack = Bool()
+  val wr_err = Bool()
+
+  override def asMaster(): Unit = {
+    out(req, req_is_wr, addr, wr_data, wr_biten)
+    in(req_stall_wr, req_stall_rd, rd_ack, rd_err, rd_data, wr_ack, wr_err)
+  }
 }
--- systemrdl_spinal/src/main/PeakrdlRegblockShim.scala
+++ systemrdl_spinal/src/main/PeakrdlRegblockShim.scala
@@ -1,6 +1,7 @@
 package systemrdl_spinal
 
 import spinal.core._
+import spinal.lib._
 
 class PeakrdlRegblockShim[T_in <: Bundle, T_out <: Bundle](
   in_type: HardType[T_in],
@@ -11,7 +12,7 @@ class PeakrdlRegblockShim[T_in <: Bundle, T_out <: Bundle](
   val io = new Bundle {
     val clk = in Bool()
     val rst = in Bool()  // This is always called rst irrespective of reset type
-    val s_cpuif = PeakrdlCpuIf(addr_width, data_width)
+    val s_cpuif = slave PeakrdlCpuIf(addr_width, data_width)
     val hwif_in = in (in_type())
     val hwif_out = out (out_type())
   }
